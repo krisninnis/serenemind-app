ea8f0338c414787115972b1ec6dfcb8ca6bb0145
Kristian Ninnis <krisninnis@gmail.com>
2025-08-05 18:38:22 +0100

    Add sessionPreferences field to User schema

--- mental-health-backend/models/User.js
+++ mental-health-backend/models/User.js
@@ -5,6 +5,14 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isVerified: { type: Boolean, default: false },
+
+    sessionPreferences: {
+      type: {
+        breathingSessionLevel: { type: String, default: "Slow Sloth" },
+        // Add more preferences here if needed later
+      },
+      default: {},
+    },
   },
   { timestamps: true }
 );
